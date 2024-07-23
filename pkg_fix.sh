ca70fc7910ed329c50a6f12bff200ad032e03bfd
hishikur <35244573+hishikur@users.noreply.github.com>
2024-07-23 17:29:37 +0900

    test

--- pkg_fix.sh
+++ pkg_fix.sh
@@ -10,5 +10,5 @@ sudo rm -rf /var/cache/yum/*
 sudo rm -rf /var/cache/dnf
 sudo yum remove katello-ca-consumer-labsat.opentlc.com -y
 sudo subscription-manager register --org=11594663 --activationkey=tempkey
-sudo subscription-manager release --set=8.4
+sudo subscription-manager release --set=8.6
 sudo insights-client --register
