95965e39a41a695ef70b74dcfdd69e4dde635f94
hishikur <35244573+hishikur@users.noreply.github.com>
2024-07-30 09:54:54 +0900

    test

---
+++ all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ansible-playbook 00_run_script.yml
+sleep 5
+sudo yum install rhel-system-roles -y
+sleep 5
+ansible-playbook 01_timesync_prep.yml
+sleep 5
+ansible-playbook 02_storage_prep.yml
+sleep 5
+ansible-galaxy collection install community.sap_install
+sleep 5
+ansible-playbook 03_hostagent_prep.yml
+sleep 5
+sudo yum install rhel-system-roles-sap -y
+sleep 5
+ansible-playbook 04_sap_prep.yml
+sleep 5
+ansible-playbook 05_hana_deploy.yml
+sleep 5
+ansible-playbook 06_hsr.yml
+#sleep 5
+#ansible-playbook 07_pacemaker.yml
+
+
